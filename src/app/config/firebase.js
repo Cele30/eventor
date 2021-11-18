d59a5ba5bcb1d93d2aba4e9f3c4b6d3576ba5d6e
Cele <celkoski.kristijan@uklo.edu.mk>
2021-11-18 17:49:38 +0100

    removed firebase config

--- src/app/config/firebase.js
+++ src/app/config/firebase.js
@@ -1,13 +1,5 @@
 import { initializeApp } from "firebase/app";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAELBzphaz30CWMNLyO9wIEmeR6Fs8J6bM",
-  authDomain: "food-order-0530.firebaseapp.com",
-  databaseURL: "https://food-order-0530-default-rtdb.firebaseio.com",
-  projectId: "food-order-0530",
-  storageBucket: "food-order-0530.appspot.com",
-  messagingSenderId: "317867731518",
-  appId: "1:317867731518:web:0c48fc7caff6c13497bf6f",
-};
+const firebaseConfig = {};
 
 export const firebase = initializeApp(firebaseConfig);
